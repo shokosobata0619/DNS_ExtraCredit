65821fa9a28437e89d5f30c24627d97130b4bf0f
shokosobata0619 <sh8444@nyu.edu>
2025-05-02 09:12:26 -0400

    Update dnsClient.py

--- dnsClient.py
+++ dnsClient.py
@@ -69,7 +69,7 @@ def dns_query(type, name, server):
         
         # Example: The QR field is located in the second byte of the DNS header, with its most significant bit being the leftmost bit of this byte. Since each byte contains 8 bits, shifting the value of the QR field left by 15 bits moves it to the correct position in the 16-bit value that represents the combination of several fields in the DNS header.
 
-    header = struct.pack('!HHHHHH', ID, QR << 15 | OPCODE << 11 | AA << 10 | TC << 9 | RD << 8| RA << 7 | Z << 4 | RCODE, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT)
+    header = struct.pack('!HHHHHH', ID, QR << 15 | OPCODE << ?? | AA << ?? | TC << ?? | RD << ??| RA << ?? | Z << ?? | RCODE, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT)
 
     # Encode the QNAME
     
@@ -81,8 +81,8 @@ def dns_query(type, name, server):
         # 3. edu (length of 3)
         
     qname_parts = name.split('.') # How can we easily split the string?
-    qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('asciix') for part in qname_parts] # Make sure it's encoded as a sequence of the right character encoding type (lowercase)
-    qname_encoded = b''.join(qname_encoded_parts) + b'\x00' #enter the closing byte value to signify the end of the domain string (two digits)
+    qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('????') for part in qname_parts] # Make sure it's encoded as a sequence of the right character encoding type (lowercase)
+    qname_encoded = b''.join(qname_encoded_parts) + b'\x??' #enter the closing byte value to signify the end of the domain string (two digits)
 
     # Encode the QTYPE and QCLASS
 
@@ -110,15 +110,15 @@ def dns_query(type, name, server):
         # It is a good idea to choose a buffer size that is large enough to accommodate the largest expected DNS response, but not so large that it wastes memory.
     
     # Parse the response header
-    response_header = data[:12] # What is the size of the DNS response header in bytes? 
+    response_header = data[:?????] # What is the size of the DNS response header in bytes? 
     ID, FLAGS, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT = struct.unpack('!HHHHHH', response_header) # We are unpacking the binary data of the response header into individual values representing the fields of the DNS header.
     
     # Parse the response question section (same as query)
-    response_question = data[12:12+len(question)] # The data variable starts immediately after the header section, so what is it's index? Note the two '??' '??' will be the same value as we start at a specific index and then go for the entire length of the binary data received. 
+    response_question = data[??:??+len(????)] # The data variable starts immediately after the header section, so what is it's index? Note the two '??' '??' will be the same value as we start at a specific index and then go for the entire length of the binary data received. 
     assert response_question == question
 
     # Parse the response answer section
-    response_answer = data[12+len(question):] # We would be looking at the same index position as before (after the header)
+    response_answer = data[??+len(question):] # We would be looking at the same index position as before (after the header)
     offset = 0
     for _ in range(ANCOUNT):
         # Parse the name
@@ -142,7 +142,7 @@ def dns_query(type, name, server):
         name = '.'.join(name_parts)
 
         # Parse the type, class, TTL, and RDLENGTH
-        type, cls, ttl, rdlength = struct.unpack('!HHIH', response_answer[offset:offset+rdlength]) # What is the offset value in bytes? Remember 'H' represent 2 bytes, and 'I' represents 4 bytes, we declared '!HHIH'. 
+        type, cls, ttl, rdlength = struct.unpack('!HHIH', response_answer[offset:offset+????]) # What is the offset value in bytes? Remember 'H' represent 2 bytes, and 'I' represents 4 bytes, we declared '!HHIH'. 
         
         offset += 10 # Same value as just calculated
 
